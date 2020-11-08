0ffc45a3df9139dce64cbd8d8bf19188d6c9693b
buivanhoa99 <buivanhoa99@gmail.com>
2020-11-08 23:45:01 +0700

    Copy project

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-var fs = require("fs");
+var fs = require('fs')
 var express = require("express");
 var app = express();
-var bodyParser = require('body-parser');
 
+var userRouter = require('./routes/user.route')
 app.use(express.static("public"));
+
+
 
 app.set("view engine","ejs");
 app.set("views","./views");
 app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+//app.use(express.urlencoded({limit: '50mb'}));
+
 var onlineUsers =[];
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
@@ -21,32 +23,14 @@ io.on("connection",function(socket){
   socket.on("client-get-files",function(){
     fs.readdir(dir, (err, files) => {
     socket.emit("server-send-files",files);
-    console.log(files);
   });
   });
 });
 
+app.use('/',userRouter)
 
 
-app.post('/', urlencodedParser, function (req, res) {
-    var pass64 = req.body.image;
-    let length=  pass64.length;
 
-    //var pass64 = Buffer.from(pass).toString('base64');
-    console.log("PASS 64:" + length);
-    let buff = new Buffer(pass64, 'base64');
-    var name  = getTime();
-    fs.writeFileSync("./public/image/"+name + ".png", buff);
-      res.send("Successful");
-    io.sockets.emit("server-send-new-file",name+".png");
-
-  });
-function getTime(){
-  var date = new Date();
-  var name = date.getHours()+"H-" + date.getMinutes()+ "M-"+date.getSeconds() +"S-"+ date.getFullYear() + "-" + date.getMonth() +"-"+date.getDate();
-  return name;
+module.exports = {
+  io
 }
-
-app.get("/",function(req,res){
-  res.render("trangchu");
-});
--- public/xuly.js
+++ public/xuly.js
@@ -1,7 +1,8 @@
 
-var socket = io("52.146.10.164");
+var socket = io("localhost:80");
 
 socket.on("server-send-files",function(data){
+    alert(111);
     for(let i=0;i<data.length;i++){
         let link = "/image/"+data[i];
 
@@ -9,14 +10,14 @@ socket.on("server-send-files",function(data){
     }
 });
 socket.on("server-send-new-file",function(data){
-
+    alert(222);
     let name = "./image/" + data;
     $("#listImage").append("<a target ='_blank' href="+name+"><img class='img' src="+name+"></a>");
 });
 
 $(document).ready(function(){
     socket.emit("client-get-files");
-    $("#a1").mousedown(function(ev){
+    $("#a1g").mousedown(function(ev){
         if(ev.which == 3)
         {
               alert("Right mouse button clicked on element with id myId");
